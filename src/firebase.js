0564432824a318bc0c938c35e703135597ce9599
deliaturner <deliajturner@gmail.com>
2021-02-21 21:54:42 -0500

    header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqu3baFgDz-voTfbR-RnXP03eLnXafHPw",
+  authDomain: "slack-lookalike-7d7ed.firebaseapp.com",
+  projectId: "slack-lookalike-7d7ed",
+  storageBucket: "slack-lookalike-7d7ed.appspot.com",
+  messagingSenderId: "133615597595",
+  appId: "1:133615597595:web:5873de42c65c9828eae0be",
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import store from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,10 +12,19 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="/__/firebase/8.2.9/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+
+// <!-- Initialize Firebase -->
+// <script src="/__/firebase/init.js"></script>
