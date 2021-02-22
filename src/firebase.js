a5f15616c33bd9c34b29ca36c1126f15a838d9e4
deliaturner <deliajturner@gmail.com>
2021-02-22 02:31:36 -0500

    sidebar

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAqu3baFgDz-voTfbR-RnXP03eLnXafHPw",
   authDomain: "slack-lookalike-7d7ed.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "133615597595",
   appId: "1:133615597595:web:5873de42c65c9828eae0be",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
